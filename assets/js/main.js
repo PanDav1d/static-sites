ca69bf58b02398bd8621a4843bdc447b410b184c
PanDav1d <70289690+PanDav1d@users.noreply.github.com>
2024-01-10 15:42:25 +0100

    adding bootstrap styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,16 @@ var app = document.getElementById("app");
 var html = document.getElementsByTagName("html")[0];
 
 function Load(id, clear) {
+    // if no value is given assign one
+    if (clear == null) clear = true;
     fetch("/content.json")
         .then(response => response.json())
         .then(json => {
+            //collapse the navbar on click
+            if (document.getElementById("navbarNavDropdown").classList.contains("show")) {
+                document.getElementById("navbarNavDropdown").classList.remove("show");
+            }
+            //check if the page should get cleared or not
             if (clear) {
                 title.innerHTML = json[0]["website-name"] + json[1]["data"][id].title;
                 app.innerHTML = json[1]["data"][id].content;
